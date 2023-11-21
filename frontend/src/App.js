9ee7c3819b90b86ddae5a5a667f5dab31f6b2dcd
Amritendu Mohan Roy <amritenduroy70@gmail.com>
2023-11-21 22:31:06 +0530

    frontend changes

---
+++ frontend/src/App.js
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+import './App.css';
+import axios from 'axios'
+import Navbar from './Navbar';
+
+function App() {
+  const [inputText, setInputText] = useState('');
+  const [resultMessage, setResultMessage] = useState('');
+  const handleInputChange = (event) => {
+    setInputText(event.target.value);
+  };
+
+  const handleButtonClick = async () => {
+    try {
+      // Format the input text as JSON
+    const formattedText = inputText
+    .replace(/\n/g, '. ') // Replace newlines with full stops
+    .replace(/"/g, "'");   // Replace double quotes with single quotes
+      // Make a POST request to localhost:8000 with the inputText
+      const response = await axios.post('http://localhost:8000/', { lyrics: formattedText });
+      console.log(response.data)
+
+      // Extract the result message from the response
+      
+
+      // Update the state with the result message
+      setResultMessage(response.data);
+    } catch (error) {
+      console.error('Error posting data:', error);
+
+      // Update the state with an error message
+      setResultMessage('Error posting data. Please try again.');
+    }
+  };
+
+  return (
+    <div>
+      <Navbar/>
+    
+    <div className='container'>
+      <div className='box'>
+        <h1>Enter your song lyrics: </h1>
+      <textarea
+        value={inputText}
+        onChange={handleInputChange}
+        placeholder="Enter lyrics here"
+      />
+      <button onClick={handleButtonClick}>Predict Genre</button>
+      <h2>The predicted Genre is: {resultMessage? resultMessage.toUpperCase()+" MUSIC" : "Provide your lyrics to check"} </h2>
+      </div>
+      
+    </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Navbar.css'
+const Navbar = () => {
+  return (
+    <div className='nav'>Music Genre Predictor Using Lyrics</div>
+  )
+}
+
+export default Navbar
